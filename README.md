d319f987ec10e86defc45a82c5486acb1adb88ca
wilian1808 <wilian.walli@gmail.com>
2020-08-04 17:19:01 -0500

    first

---
+++ README.md
@@ -0,0 +1,33 @@
+# TAREA RESTRICCIONES DE INTEGRIDAD.
+
+Para poder realizar el ejercicio se creo una base de datos llamado `WorkExampleDB` se renombro a esta para evitar que tenga conflictos con alguna base de datos ya existente.
+
+```sql
+DROP DATABASE IF EXISTS WorkExampleDB;
+
+CREATE DATABASE WorkExampleDB;
+
+USE WorkExampleDB;
+```
+
+> Borramos la base de datos *WorkExampleDB* si es que ya existe luego creamos la base de datos *WorkExampleDB* y la usamos para ejecutar los siguientes codigos *sql*.
+
+#### 1. Primero.
+
+Crear una tabla `estudiantes`, los campos debe de incluir un id entero autoincrementable, número de matricula y dni con restricciones de formato y únicos, cuenta de correo electronico con la restriccion de formato correspondiente, debe de incluirse campos para sexo, fecha de nacimiento, apellidos y nombres. Inserte datos de su familia.
+
+```sql
+
+
+```
+
+#### 2. Segundo
+
+Segundo. Crear una tabla para matrícula y otra para curso, debe considerarse las restricciones de
+integridad referencial.
+
+#### 3. tercero.
+
+Indagar como acceder remotamente a una base de datos. Probar con alguno de sus
+compañeros de clase.
+Adjuntar capturas de pantalla.
---
+++ file.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS WorkExampleDB;
+
+CREATE DATABASE WorkExampleDB;
+
+USE WorkExampleDB;
+
+CREATE TABLE Estudiantes (
+    EstudianteID INTEGER NOT NULL IDENTITY(1, 1),
+    Nombres VARCHAR(100) NOT NULL,
+    Apellidos VARCHAR(100) NOT NULL,
+    NumeroMatricula INTEGER NOT NULL,
+    Dni INTEGER NOT NULL,
+    Correo VARCHAR (100) NOT NULL,
+    Sexo VARCHAR (20) NOT NULL ,
+    FechaNacimiento DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_estudiantes PRIMARY KEY (EstudianteID),
+    CONSTRAINT uk_numero_matricula_estudiantes UNIQUE (NumeroMatricula),
+    CONSTRAINT ck_numero_matricula_estudiantes CHECK (NumeroMatricula > 0),
+    CONSTRAINT uk_dni_estudiantes UNIQUE (Dni),
+    CONSTRAINT ck_dni_estudiantes CHECK (Dni > 0),
+    CONSTRAINT uk_correo_estudiantes UNIQUE (Correo)
+);
+
+INSERT INTO Estudiantes (Nombres, Apellidos, NumeroMatricula, Dni, Correo, Sexo) VALUES ('mario', 'ccama ccama', 202020, 78487548, 'mario@gmail.com', 'masculino');
+
+--
+CREATE TABLE cursos (
+    id_curso INT NOT NULL IDENTITY,
+    nombre VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_cursos PRIMARY KEY (id_curso),
+    CONSTRAINT uk_cursos_nombre UNIQUE (nombre)
+);
+
+CREATE TABLE matriculas (
+    id_matricula INT NOT NULL IDENTITY,
+    id_estudiante INT NOT NULL,
+    id_curso INT NOT NULL,
+    CONSTRAINT pk_matriculas PRIMARY KEY (id_matricula),
+    CONSTRAINT fk_matriculas_estudiantes FOREIGN KEY (id_estudiante) REFERENCES estudiantes (id_estudiante)
+        ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT fk_matriculas_cursos FOREIGN KEY (id_curso) REFERENCES cursos (id_curso)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+);
